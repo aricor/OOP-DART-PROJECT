6193cd68bebba5dd40fb5abce509c0461df8f5c3
amandaturquis <71436509+amandaturquis@users.noreply.github.com>
2020-09-22 10:47:35 +0200

    CustomerMenu

---
+++ CustomerMenu.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class CustomerMenu {
+
+    CustomerMenu () {
+
+    }
+    public void displayCustomerMenu() {
+        Scanner inputCustomer = new Scanner(System.in);
+
+        System.out.println("Customer Screen - Type one of the options below:");
+        System.out.println("1. Rent a game");
+        System.out.println("2. Return a game");
+        System.out.println("3. Return to Main Menu");
+
+        int customerChoice = inputCustomer.nextInt();
+        inputCustomer.close();
+        if (customerChoice == 1) {
+            //Rent a game
+
+        } else if (customerChoice == 2) {
+            //Return a game
+
+        } else if (customerChoice == 3){
+            new MainMenu();
+        } else {
+            System.out.println("Please choose an option from the menu"); //Wrong input
+        }
+    }
+}
